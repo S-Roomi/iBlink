59ff68245a0023e5e7b378603ace21704fb43802
Sina Roomi <sroomi2004@gmail.com>
2026-04-07 11:23:13 -0400

    Websocket setup and working. Can detect and update eyeblink counter

--- my-app/src/BlinkDetector.tsx
+++ my-app/src/BlinkDetector.tsx
@@ -1,52 +1,65 @@
-import React, { useState, useEffect, useRef } from "react";
-import type { SessionStatus, Device, StatusResponse} from "./types"
+import { useState, useEffect, useRef, useCallback } from "react";
+import type { Device, StatusResponse} from "./types"
 import "./App.css";
 
 const BLINK_THRESHOLD = 10;
+
+// Connect websocket
+const useConnectWS = (setTemp: (data:any) => void ) => {
+  const ws = useRef<WebSocket | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+
+  const connect = useCallback(() => {
+    if (ws.current?.readyState == WebSocket.OPEN) return //already open
+
+    ws.current = new WebSocket('ws://localhost:8000/ws/live')
+
+    ws.current.onopen = () => setIsConnected(true);
+
+    ws.current.onmessage = (event) => {
+      const data: any = JSON.parse(event.data);
+      console.log(data?.data)
+      setTemp(data?.data);
+    };
+    
+    ws.current.onclose = () => setIsConnected(false);
+  }, [setTemp])
+
+  const disconnect = useCallback(() => {
+    ws.current?.close()
+  }, [])
+
+
+  return { connect, disconnect, isConnected }; 
+};
+
 
 export default function BlinkDetector() {
   const [blinks, setBlinks] = useState<number[]>([]);
   const [alert, setAlert] = useState(false);
 
   const [devices, setDevices] = useState<Device[]>([]);
-  //const [status, setStatus] = useState<SessionStatus | null>(null);
-  const [isConnected, setIsConnected] = useState<boolean>(false);
 
   const [temp, setTemp] = useState<StatusResponse | null>(null);
 
-  const ws = useRef<WebSocket | null>(null);
+  const { connect, disconnect,  isConnected } = useConnectWS(setTemp)
 
-  // Fetch all devices on mount
+
+  // Fetch devices on start
   useEffect(() => {
     fetch('http://localhost:8000/api/devices').then(res => res.json()).then(setDevices);
   }, []);
 
-  // Connect websocket
-  const connectWS = () => {
-
-    useEffect(() => {
-      ws.current = new WebSocket('ws://localhost:8000/ws/live'); 
-      ws.current.onopen = () => setIsConnected(true);
-      ws.current.onmessage = (event) => {
-        const data: StatusResponse = JSON.parse(event.data);
-        setTemp(data);
-      }
-      ws.current.onclose = () => setIsConnected(false);
-    }, [])
-    
-  }
-
   // API Command Wrappers
+
   const startSession = async () => {
     await fetch('http://localhost:8000/api/session/start', { method: 'POST' });
-    connectWS(); // Connect WS when session starts
+    connect()
   };
 
   const stopSession = async () => {
     await fetch('http://localhost:8000/api/session/stop', { method: 'POST' });
-    
-    //check to see if ws is open. If so close it
-    ws.current?.close();
+    disconnect()
   };
 
   const dismissAlert = () => {
@@ -65,24 +78,12 @@ export default function BlinkDetector() {
     fetch('http://localhost:8000/api/session/chart', { method: 'GET'})
   }
 
-  const handleBlink = () => {
-    const now = Date.now();
-    setBlinks(prev => {
-      const updated = [...prev, now];
-      if (updated.length >= BLINK_THRESHOLD) setAlert(true);
-      return updated;
-    });
-  };
-  const resetBlink = () => {
-    setBlinks([])
-    setAlert(false)
-  }
-
-
     return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#13131a]">
       <h1 className="blinking text-6xl font-bold mb-6 text-gray-100 text-center">i-Blink</h1>
       <div className="bg-[#181820] rounded-xl shadow-lg px-10 py-12 flex flex-col items-center w-full max-w-md">
+        <p className="text-white text-lg"> {isConnected ? "Live" : "Offline"}</p>
+        {/* <p className="text-white text-lg"> {devices ? JSON.stringify(devices) : ""}</p> */}
         <button
           className="mb-6 px-8 py-3 bg-green-500 hover:bg-green-600 text-white text-md font-semibold rounded transition"
           onClick={startSession}
@@ -121,39 +122,19 @@ export default function BlinkDetector() {
         >
           Chart
         </button>
+
+        <button 
+          className="mb-6 px-8 py-3 bg-teal-500 hover:bg-teal-600 text-white text-sm font-semibold rounded transition"
+          onClick={() => {
+            stopSession
+            setTemp(null)
+          }
+          }>
+          Reset
+        </button>
       </div>
     </div>
   );
-
-  // return (
-  //   <div className="p-4 border rounded-lg shadow-md">
-  //     <h2 className="text-xl font-bold mb-4">Acoustic Blink Detector</h2>
-      
-  //     <div className="space-x-2 mb-6">
-  //       <button onClick={startSession} className="bg-green-500 text-white px-4 py-2 rounded">Start</button>
-  //       <button onClick={stopSession} className="bg-red-500 text-white px-4 py-2 rounded">Stop</button>
-  //       <button onClick={dismissAlert} className="bg-gray-500 text-white px-4 py-2 rounded">Dismiss Alert</button>
-  //     </div>
-
-  //     {/* Visual Feedback */}
-  //     <div className={`p-4 rounded ${status?.alert_triggered ? 'bg-red-200 animate-pulse' : 'bg-blue-50'}`}>
-  //       <p>Status: {isConnected ? "🟢 Connected" : "🔴 Disconnected"}</p>
-  //       <p>Alert State: <strong>{status?.alert_triggered ? "BLINK DETECTED" : "Normal"}</strong></p>
-  //     </div>
-
-  //     {/* Real-time Chart Image */}
-  //     {isConnected && (
-  //       <div className="mt-4">
-  //         <h3 className="font-semibold">Live Signal:</h3>
-  //         <img 
-  //           src={`http://localhost:8000/api/session/chart?t=${Date.now()}`} 
-  //           alt="FMCW Signal" 
-  //           className="w-full border"
-  //         />
-  //       </div>
-  //     )}
-  //   </div>
-  // );
 }
 
 
